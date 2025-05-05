1a4e11835ef7396b421f2f7fc85013cd2fe94ddd
Alex Bezkopylnyi <sanok.bes@gmail.com>
2025-05-05 11:41:07 +0100

    Initial Commit

---
+++ SmartShopper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartShopper
+//
+//  Created by AlexBezkopylnyi on 05.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartShopper/SmartShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShopperApp.swift
+//  SmartShopper
+//
+//  Created by AlexBezkopylnyi on 05.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SmartShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
